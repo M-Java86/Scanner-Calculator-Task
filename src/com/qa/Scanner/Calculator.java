08d30921bc30ac35c1bdce60abf18cc88c2b8d13
M-java86 <mpythondata@outlook.com>
2021-05-25 15:55:18 -0400

    MO: added Task instructions

--- src/com/qa/Scanner/Calculator.java
+++ src/com/qa/Scanner/Calculator.java
@@ -6,5 +6,24 @@ public class Calculator {
 		// TODO Auto-generated method stub
 
 	}
+	
+	
 
 }
+
+
+
+
+
+
+
+//Scanner Task 
+//Create a project that has four methods; addition, subtraction, multiplication, and division:
+
+//All of these methods should take two numbers as parameters.
+//Create a menu to ask the user which of the four methods they wish to use, then take the numbers as user input for the operation and output the result back to the user.
+
+
+//Stretch Goal 
+//Stretch goal: Using java -jar + maven, use the menu through the CLI.
+
