0c7d7b10315bf8849aa3acefcf07b753e5b1d0de
M-java86 <mpythondata@outlook.com>
2021-05-25 15:59:00 -0400

    MO: added operation goals

--- src/com/qa/Scanner/Calculator.java
+++ src/com/qa/Scanner/Calculator.java
@@ -4,6 +4,14 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Operations Goals
+		// -Addition
+		// - Subtraction 
+		//- Division
+		// -Multiplication
+		// - Exit
+		
 
 	}
 	
