141c58973aaa30dd16e1b93933e9a8e6bf34d87c
M-java86 <mpythondata@outlook.com>
2021-05-26 01:44:21 -0400

    MO:Caculator Task

--- src/com/qa/Scanner/Calculator.java
+++ src/com/qa/Scanner/Calculator.java
@@ -1,29 +1,91 @@
 package com.qa.Scanner;
 
+
+import java.util.Scanner;
+
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static Scanner scanner = new Scanner(System.in);
+	
+	//@SuppressWarnings("resource")
+	public static void main(String[] args ) {
 		
-		//Operations Goals
-		// -Addition
+		
+		
+		
+		double int1, int2;
+			System.out.println("Enter first number:");
+			
+			//using data type double so that user can 
+			//enter integer as well as floating point value. 
+		
+		
+		double int1 = scanner.nextDouble();
+		System.out.println("Enter second number:");
+		 double int2 = scanner.nextDouble();
+		
+		
+		System.out.println("Enter an operator (+, -, *, /): ");
+		char operator1 = scanner.nextLine().charAt(0);
+		
+		scanner.close();
+		double output;
+		
+		
+		
+			switch(operator1)
+			{
+			
+			case '+':
+		
+				 output = operator1 +  operator1;
+				break;
+		
+		
+			case '-':
+				 output = operator1 - operator1;
+				break;
+				
+			 
+			case  '*':
+				 output = operator1 * operator1;
+				 
+			
+			case '/':
+				output = operator1 / operator1;
+				break;
+				
+			dafault:
+				System.out.println("You have entered the wrong operator");
+			
+		
+		
+	}
+			
+		System.out.println(num1+" "operator+" "+operator1+ +"+num2": "+output");
+			
+	}
+	
+}
+			
+	
+
+	
+		
+		
+
+
+		
+
+
+
+
+//Operations Goals
+		// -Addition 
 		// - Subtraction 
 		//- Division
 		// -Multiplication
 		// - Exit
-		
-
-	}
-	
-	
-
-}
-
-
-
-
-
-
 
 //Scanner Task 
 //Create a project that has four methods; addition, subtraction, multiplication, and division:
